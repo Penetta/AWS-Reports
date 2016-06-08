0d4ecda61cc81343ef6b488e31d54724e9ace0e2
Eduardo Penetta <eduardo@penetta.com>
2016-06-08 11:21:44 -0300

    AWS
    AWS

--- ec2-vale_scan.py
+++ ec2-vale_scan.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-#  Desenvolvimento Penetta
+#  Desenvolvimento Eduardo Penetta
 #  Date: 07/06/2016
 #  CSC 
 #
-
 
 import boto.s3
 import boto.rds
 import boto.ec2
 import boto.ec2.elb
+import boto.vpc
 import getopt
 import sys
 import time
@@ -33,11 +33,11 @@ def usage():
     msg = """
 
     -o --option 	      Option [ scan_ec2 | scan_rds | scan_s3 | scan_images | scan_snapshot | scan_volumes 
-                                       scan_security_group | scan_loadbalance | scan_all_address | scan_all_key_pairs ]
+                                       scan_security_group | scan_loadbalance | scan_all_address | scan_all_key_pairs | scan_all_aws ]
 
     -r --region   	      AWS Region (us-east-1 = USA / sa-east-1 = BR / us-west-2 = OREGON )
-    -i --id                   AWS Access Key ID
-    -k --key                  AWS Secret Access Key
+    -i --id               AWS Access Key ID
+    -k --key              AWS Secret Access Key
  
     Exemplo:
  
@@ -70,18 +70,10 @@ for opt, arg in options:
         id = arg
     elif opt in ('-k', '--key'):
         key = arg
-    elif opt in ('-s', '--server'):
-        server = arg
     elif opt in ('-o', '--option'):
         option = arg
-    elif opt in ('-a', '--address'):
-        address = arg 
-    elif opt in ('-f', '--filter'):
-        filter = arg 
     elif opt in ('-d', '--days'):
         day = arg 
-    elif opt in ('-c', '--account'):
-        account = arg 
     elif opt in ('-h', '--help'):
         usage()
     else: 
@@ -97,22 +89,25 @@ else:
   print 'REGION    :', region
   print 'ID        :', id
   print 'KEY       :', key
-  #print 'SERVER ID :', server
   print 'OPTION    :', option
-  #print 'ADDRESS IP:', address
-  #print 'FILTER    :', filter
-  #print 'DAYS      :', day
+
 
 #### Conecte AWS 
 conn = boto.ec2.connect_to_region(region,aws_access_key_id=id,aws_secret_access_key=key)
 conn_rds = boto.rds.connect_to_region(region,aws_access_key_id=id,aws_secret_access_key=key)
 conn_elb = boto.ec2.elb.connect_to_region(region,aws_access_key_id=id,aws_secret_access_key=key)
 conn_s3  = boto.s3.connect_to_region(region,aws_access_key_id=id,aws_secret_access_key=key)
+conn_vpc = boto.vpc.connect_to_region(region,aws_access_key_id=id,aws_secret_access_key=key)
 
-def send_alert_gmail(conteudo):
+def send_alert_gmail(scan_ec2,scan_images,scan_volumes,scan_snapshot,scan_security_group,scan_loadbalance,scan_rds,scan_s3,scan_all_address,scan_all_key_pairs,scan_all_vpc):
+
    import smtplib
    from email.MIMEMultipart import MIMEMultipart
    from email.MIMEText import MIMEText
+
+   # Date time
+   now = datetime.datetime.now()
+   date_time = now.strftime("%Y-%m-%d %H:%M")
 
    # Specifying the from and to addresses
 
@@ -124,10 +119,73 @@ def send_alert_gmail(conteudo):
    msg['To'] = toaddrs
    msg['Subject'] = "REPORT AWS TODAY"
 
-
    # Writing the message (this message will appear in the email)
 
-   body = conteudo
+   body = """                                                                                                                                                              <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://zabbix.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html xmlns='http://zabbix.w3.org/1999/xhtml'><head>
+<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
+<title>CSC - AWS</title>
+</head>
+
+<body>
+<table width='555' border='0' cellspacing='0' cellpadding='0'>
+  <tr>
+    <td colspan='2'><img src='http://zabbix.cscm3.com.br/alerta/cabecalho1.jpg' width='555' height='49'></td>
+  </tr>
+  <tr>
+    <td width='38' height='40' style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/EC2.png' width='23' height='23'></td>
+    <td width='517' style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat; padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total EC2: %s</td>
+  </tr>   
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/ELASTIC.png' width='23' height='22' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total Elastic IP: %s</td>
+</tr>
+<tr>
+    <td height='40' style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/KEYPAIRS.png' width='23' height='20'></td>
+    <td height='40' style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total Key Pairs: %s</td>
+  </tr>
+
+<tr>
+    <td height='40' style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/IMAGES.png' width='23' height='20'></td>
+    <td height='40' style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total AMIs: %s</td>
+  </tr>
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/VOLUMES.png' width='23' height='22' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total Volumes: %s</td>
+</tr>
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/SNAPSHOT.png' width='23' height='22' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total Snapshot: %s</td>
+</tr>
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/SECURITY.png' width='23' height='22' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total Security Group: %s</td>
+</tr>
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/LOADBALANCE.png' width='23' height='22' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total Load Balancers: %s</td>
+</tr>
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/VPC.png' width='26' height='18' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total VPC: %s</td>
+</tr>
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/RDS.png' width='22' height='22' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total RDS: %s</td>
+</tr>
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/S3.png' width='23' height='22' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'>Total S3 Bucket: %s</td>
+</tr>
+<tr>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:10px;'><img src='http://zabbix.cscm3.com.br/alerta/DATE.png' width='23' height='22' /></td>
+    <td height='40'  style='background-image:url(http://zabbix.cscm3.com.br/alerta/bg.jpg); background-repeat:no-repeat;  padding-left:3px;padding-left:3px; font-family:Verdana, Geneva, sans-serif; font-size:14px;'><b>Atualizado: %s By Penetta</b></td>
+</tr>
+</table>
+</body>
+</html>""" % (scan_ec2,scan_all_address,scan_all_key_pairs,scan_images,scan_volumes,scan_snapshot,scan_security_group,scan_loadbalance,scan_all_vpc,scan_rds,scan_s3, date_time) 
+
+
+
    #msg.attach(MIMEText(body, 'plain'))
    msg.attach(MIMEText(body, 'html'))
    
@@ -156,8 +214,8 @@ def scan_ec2 ():
          name = inst.tags['Name'] if 'Name' in inst.tags else 'Unknown'
          print "%s (%s) [%s] {%s} [%s]" % (name, inst.id, inst.state, inst.image_id, inst.ip_address   )
 
-   result = " ---- Total de EC2 = %s ----"  % len(list_instances)
-   print " ---- Total de EC2 = %s ----"  % len(list_instances)
+   result = "%s"  % len(list_instances)
+   print " ' ---- Total 'de EC2 = %s ----"  % len(list_instances)
    return result
 
 def scan_images ():
@@ -166,7 +224,7 @@ def scan_images ():
     for images in list_imagens:
           print "Imagens: (%s) - %s  - [%s] - {%s} (%s) ... " % (images.id,images.name, images.description,images.state,images.location)
 
-    result = " ---- Total de Imagens = %s ----"  % len(list_imagens)
+    result = "%s"  % len(list_imagens)
     print " ---- Total de Imagens = %s ----"  % len(list_imagens)
     return result
 
@@ -176,7 +234,7 @@ def scan_volumes():
     for volume in list_volumes:
         print "Volumes: "+ volume.id, volume.status
 
-    result = " ---- Total de Volumes = %s ----"  % len(list_volumes)
+    result = "%s" % len(list_volumes)
     print " ---- Total de Volumes = %s ----"  % len(list_volumes)
     return result
 
@@ -186,7 +244,7 @@ def scan_snapshot():
     for snapshot in list_snapshot:
         print "SnapShot: "+ snapshot.id, snapshot.status
 
-    result = " ---- Total de SnapShot = %s ----"  % len(list_snapshot)
+    result = "%s"  % len(list_snapshot)
     print " ---- Total de SnapShot = %s ----"  % len(list_snapshot)
     return result
 
@@ -196,7 +254,7 @@ def scan_security_group():
     for security in list_security:
         print "Security Group: "+ security.name
 
-    result = " ---- Total de Security Group = %s ----"  % len(list_security)
+    result = "%s"  % len(list_security)
     print " ---- Total de Security Group = %s ----"  % len(list_security)
     return result
 
@@ -206,7 +264,7 @@ def scan_loadbalance():
     for load in list_loadbalance:
         print "Load Balance: "+ load.id
 
-    result = " ---- Total de Load Balance = %s ----"  % len(list_loadbalance)
+    result = "%s"  % len(list_loadbalance)
     print " ---- Total de Load Balance = %s ----"  % len(list_loadbalance)
     return result
 
@@ -216,7 +274,7 @@ def scan_rds():
     for rds in list_rds:
         print "RDS : "+ rds.name
 
-    result = " ---- Total de RDS = %s ----"  % len(list_rds)
+    result = "%s"  % len(list_rds)
     print " ---- Total de RDS = %s ----"  % len(list_rds)
     return result
 
@@ -229,18 +287,13 @@ def sizeof(num):
 def scan_s3():
 
     list_s3 = conn_s3.get_all_buckets()
+
     #print list_s3
     for s3 in list_s3:
     
-        #bucket = conn_s3.lookup(s3.name)
-        #total_bytes = 0
-        #for key in bucket:
-        #    total_bytes += key.size
-        #print sizeof(total_bytes)
-
         print "S3 Bucket: %s " %  (s3.name)
     
-    result = " ---- Total de S3 Bucket = %s ----"  % len(list_s3)
+    result = "%s"  % len(list_s3)
     print " ---- Total de S3 Bucket = %s ----"  % len(list_s3)
     return result
 
@@ -251,7 +304,7 @@ def scan_all_address():
     for address in list_address:
         print "Address: "+ address.public_ip
 
-    result = " ---- Total de All Address = %s ----"  % len(list_address)
+    result = "%s"  % len(list_address)
     print " ---- Total de All Address = %s ----"  % len(list_address)
     return result
 
@@ -261,10 +314,19 @@ def scan_all_key_pairs():
     for keypairs in list_keypairs:
         print "Key Pairs: "+ keypairs.name
 
-    result = " ---- Total de All Key Pairs = %s ----"  % len(list_keypairs)
+    result = "%s"  % len(list_keypairs)
     print " ---- Total de All Key Pairs = %s ----"  % len(list_keypairs)
     return result
 
+def scan_all_vpc():
+
+    list_vpc = conn_vpc.get_all_vpcs()
+    for vpc in list_vpc:
+        print "Key Pairs: "+ vpc.id
+
+    result = "%s"  % len(list_vpc)
+    print " ---- Total de All VPCs = %s ----"  % len(list_vpc)
+    return result
 
 ##### CHAMANDO AS FUNCOES
 if option in ('scan_ec2'):
@@ -297,35 +359,17 @@ elif option in ('scan_all_address'):
 elif option in ('scan_all_key_pairs'):
     scan_all_key_pairs()
 
-elif option in ('scan_all_aws'):
-   scan_all  = '<b>%s</b> \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s'  % (scan_ec2(),scan_images(),scan_volumes(),scan_snapshot(),scan_security_group(),scan_loadbalance(),scan_rds(),scan_s3(),scan_all_address(),scan_all_key_pairs())
- 
-   print scan_all
-   
-   send_alert_gmail(scan_all)
+elif option in ('scan_all_vpc'):
+    scan_all_vpc()
 
+elif option in ('scan_all_aws'):
+   #scan_all  = '<b>%s</b> \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n %s'  % (scan_ec2(),scan_images(),scan_volumes(),scan_snapshot(),scan_security_group(),scan_loadbalance(),scan_rds(),scan_s3(),scan_all_address(),scan_all_key_pairs(), scan_all_vpc())
+   send_alert_gmail (scan_ec2(),scan_images(),scan_volumes(),scan_snapshot(),scan_security_group(),scan_loadbalance(),scan_rds(),scan_s3(),scan_all_address(),scan_all_key_pairs(), scan_all_vpc())
+ 
 else:
    #print "OPCAO ESCOLHIDA ERRADA!"
    print "----  CAMPOS INVALIDOS --" 
    usage()
 
 
-### DELET IMAGEM
-   #images = connection.get_all_images(image_ids=['ami-cf86xxxx'])
-   #images[0].deregister()
-
-### STATUS INSTANCE
-  #instance = conn.get_all_instances(instance_ids=['instance_id'])
-  #print instance[0].instances[0].start()
-
-### STOP INSTANCE
-   #conn.stop_instances(instance_ids=['instance-id-1','instance-id-2', ...])
-
-### START INSTANCE
-   #conn.start_instances(instance_ids=['instance-id-1','instance-id-2', ...])
-   # Sleep for a few seconds to ensure starting
-   #sleep(10)
-   # Associate the Elastic IP with instance 
-   #if ip:
-     # conn.associate_address(instance, ip)
 
